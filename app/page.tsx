b303f51bee656db86dab5f6112c3d694df800602
Martin Arce <martin.arce.teixeira@gmail.com>
2024-02-09 21:29:27 +0100

    Assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Page() {
   return (
+    <div className="h-[100vh] w-full flex justify-center items-center">
       <h1 className="text-white">Font XL</h1>
+    </div>
   );
 }
